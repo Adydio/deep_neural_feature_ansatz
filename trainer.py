622b9ec9fe5896de1f2c3d36b90a972aac7e7555
Yukun Dong <91249962+Adydio@users.noreply.github.com>
2025-09-19 19:53:17 -0400

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -104,6 +104,8 @@ def select_optimizer(name, lr, net, weight_decay):
     if name == 'sgd':
         return torch.optim.SGD(net.parameters(), lr=lr, weight_decay=weight_decay)
     elif name == 'adam':
+        return torch.optim.Adam(net.parameters(), lr=lr, weight_decay=weight_decay)
+    elif name == 'adamw':
         return torch.optim.AdamW(net.parameters(), lr=lr, weight_decay=weight_decay)
     elif name == 'muon':
         return Muon(list(net.parameters()), lr=lr, momentum=0.6, nesterov=True, weight_decay=weight_decay)
